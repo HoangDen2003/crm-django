743ad67e201f4a5fa1ad12d40d1b2c1983a60ce6
HoangDen2003 <hadinhhoang031104@gmail.com>
2025-02-21 15:58:18 +0700

    make it better

--- crmdjango/settings.py
+++ crmdjango/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['crm-django-b12b8da3ba4c.herokuapp.com']
 
 
 # Application definition
